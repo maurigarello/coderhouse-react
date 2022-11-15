cc161cf9c7cdf4d7a514dc21a8252e107c961579
Mauricio Garello <mauriciogarellog2g@gmail.com>
2022-11-14 21:25:46 -0300

    cambios

--- Ecommerce/src/App.js
+++ Ecommerce/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>!Hola Mundo!</h1>
+      <Navbar />
     </div>
   );
 }
